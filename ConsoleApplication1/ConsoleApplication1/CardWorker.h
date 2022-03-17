8f296fa3065fbb4f2ada07e8cd9a32d7f71b6431
navys00 <73278416+navys00@users.noreply.github.com>
2022-03-17 13:21:22 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/CardWorker.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+#include"Tqueue.h"
+#include <iostream>
+#include<fstream>
+struct Patient {
+	std::string FIO;
+	std::string BornDate;
+	bool Gender;
+
+};
+class CardWorker
+{
+private:
+	Tqueue<Patient> *_pQueuePatient;
+
+	std::string _path = "PatientDatabase\\patient_";
+	void ParseCard() {
+		std::fstream fin;
+		bool fileCheck=true;
+		for (int i = 0;; i++) {
+			std::string fullpath=this->_path + std::to_string(i)+".txt";
+			fin.open(fullpath);
+			if (fin.is_open()) {
+				std::string fio;
+				std::string borndate;
+				std::string gender;
+				std::getline(fin, fio);
+				std::getline(fin, borndate);
+				std::getline(fin, gender);
+				Patient p;
+				p.BornDate = borndate;
+				p.FIO = fio;
+				p.Gender = gender == "m" ? false : true;
+				_pQueuePatient->Push(p);
+				fin.close();
+			}
+			else {
+				break;
+			}
+		}
+	}
+public:
+	CardWorker(Tqueue<Patient>& pQueuePatient) {
+		_pQueuePatient = &pQueuePatient;
+		ParseCard();
+	}
+
+
+
+};
+
---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include<fstream>
+#include <Windows.h>
+#include<string>
+#include"patient_card.h"
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus"); // вызов функции настройки локали
+    //patient_card PC = patient_card(10);
+    Tqueue<int> s = Tqueue<int>(10);
+    s.Push(1);
+    s.Print();
+
+}
+
