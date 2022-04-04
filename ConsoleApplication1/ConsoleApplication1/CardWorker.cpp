293165498b2b8bb26f841af5c42703869cce7c37
navys00 <73278416+navys00@users.noreply.github.com>
2022-04-05 00:13:14 +0300

    Update CardWorker.cpp

--- ConsoleApplication1/ConsoleApplication1/CardWorker.cpp
+++ ConsoleApplication1/ConsoleApplication1/CardWorker.cpp
@@ -1,1 +1,55 @@
-#include "CardWorker.h"
+#pragma once
+#include <string>
+#include"TQueue.h"
+#include <iostream>
+#include<fstream>
+struct Patient {
+    std::string FIO;
+    std::string BornDate;
+    bool Gender;
+
+};
+class CardWorker
+{
+public:
+    TQueue<Patient>* _pQueuePatient;
+
+    std::string _path = "PatientDatabase\\patient_";
+    void ParseCard() {
+        std::fstream fin;
+       // bool fileCheck = true;
+        std::string fio;
+        std::string borndate;
+        std::string gender;
+        for (int i = 0;; i++) {
+            std::string fullpath = this->_path + std::to_string(i) + ".txt";
+            fin.open(fullpath);
+            if (fin.is_open()) {
+                
+                std::getline(fin, fio);
+                std::getline(fin, borndate);
+                std::getline(fin, gender);
+                Patient p;
+                p.BornDate = borndate;
+                p.FIO = fio;
+                p.Gender = gender == "m" ? false : true;
+                _pQueuePatient->Push(p);
+                fin.close();
+            }
+            else {
+                break;
+            }
+        }
+
+    }
+    CardWorker() {
+        _pQueuePatient = new TQueue<Patient>(10);
+        ParseCard();
+    }
+
+    
+
+
+};
+
+
